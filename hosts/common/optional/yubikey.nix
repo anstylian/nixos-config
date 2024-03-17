e72a21385733d2135283d55ad737024b4821608e
Angelos Stylianidis <agathangelos.stylianidis@gmail.com>
2024-03-18 01:20:30 +0200

    smart card read

--- hosts/common/optional/yubikey.nix
+++ hosts/common/optional/yubikey.nix
@@ -1,9 +1,12 @@
 { pkgs, ... }: {
+
+  # smart card support
+  services.pcscd.enable = true;
+
   environment.systemPackages = with pkgs; [
     gnupg
     yubico-pam
     yubioath-flutter
     age-plugin-yubikey
   ];
-
 }
