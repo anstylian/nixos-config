9466c608715e4021bfc00397564f4cb4fa6ef372
Angelos Stylianidis <agathangelos.stylianidis@gmail.com>
2024-04-22 19:52:09 +0300

    clear config

--- home/angelos/features/desktop/common/wayland-wm/swaylock.nix
+++ home/angelos/features/desktop/common/wayland-wm/swaylock.nix
@@ -7,9 +7,6 @@ in
     enable = true;
     package = pkgs.swaylock-effects;
     settings = {
-      effect-blur = "20x3";
-      fade-in = 0.1;
-
       font = config.fontProfiles.regular.family;
       font-size = 24;
 
