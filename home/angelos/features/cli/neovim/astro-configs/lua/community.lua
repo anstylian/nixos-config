0e5ee3f8ed09561e77d0b1e1d1f5395de3bc65fd
Angelos Stylianidis <agathangelos.stylianidis@gmail.com>
2024-05-17 09:42:10 +0300

    setting up monokai. testdrive monokai modifications

--- home/angelos/features/cli/neovim/astro-configs/lua/community.lua
+++ home/angelos/features/cli/neovim/astro-configs/lua/community.lua
@@ -8,5 +8,13 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.note-taking.venn-nvim" },
+  {
+      "loctvl842/monokai-pro.nvim",
+      config = function()
+          require("monokai-pro").setup {
+          filter = "machine", -- classic | octagon | pro | machine | ristretto | spectrum
+      }
+      end
+  },
   -- import/override with your plugins folder
 }
