95562685f086d1f90c34a2bd6ba6cf3389f47b79
Angelos Stylianidis <agathangelos.stylianidis@gmail.com>
2024-05-17 11:07:18 +0300

    tmux: when splitting a window,  open on the same dir as the previous window

--- home/angelos/features/cli/tmux.nix
+++ home/angelos/features/cli/tmux.nix
@@ -16,18 +16,18 @@
           {
             plugin = dracula;
             extraConfig = ''
-    # available plugins: battery, cpu-usage, git, gpu-usage, ram-usage, tmux-ram-usage, network, network-bandwidth, network-ping,
-    # ssh-session, attached-clients, network-vpn, weather, time, mpc, spotify-tui, kubernetes-context, synchronize-panes
-    set -g @dracula-plugins "git battery time"
+              # available plugins: battery, cpu-usage, git, gpu-usage, ram-usage, tmux-ram-usage, network, network-bandwidth, network-ping,
+              # ssh-session, attached-clients, network-vpn, weather, time, mpc, spotify-tui, kubernetes-context, synchronize-panes
+              set -g @dracula-plugins "git battery time"
 
-    set -g @dracula-show-battery true
-    set -g @dracula-show-powerline true
-    set -g @dracula-refresh-rate 10
-    set -g @dracula-military-time true
+              set -g @dracula-show-battery true
+              set -g @dracula-show-powerline true
+              set -g @dracula-refresh-rate 10
+              set -g @dracula-military-time true
 
-    # it can accept `hostname` (full hostname), `session`, `shortname` (short name), `smiley`, `window`, or any character.
-    set -g @dracula-show-left-icon 
-  '';
+              # it can accept `hostname` (full hostname), `session`, `shortname` (short name), `smiley`, `window`, or any character.
+              set -g @dracula-show-left-icon 
+            '';
           }
           {
             plugin = better-mouse-mode;
@@ -37,6 +37,8 @@
           }
         ];
       extraConfig = ''
+        bind-key '"' split-window -c "#{pane_current_path}"
+        bind-key % split-window -h -c "#{pane_current_path}"
       '';
     };
   };
