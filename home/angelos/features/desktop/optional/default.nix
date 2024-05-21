93513e17f87df160aefe6e633c06bf2f3747c077
Angelos Stylianidis <agathangelos.stylianidis@gmail.com>
2024-05-21 21:18:20 +0300

    add d-spy

--- home/angelos/features/desktop/optional/default.nix
+++ home/angelos/features/desktop/optional/default.nix
@@ -1,6 +1,7 @@
 { pkgs, ... }: {
   home.packages = with pkgs; [
-    obsidian # Time managment
-    signal-desktop # Messaging app
+    obsidian        # Time managment
+    signal-desktop  # Messaging app
+    d-spy           # Monitor dbus
   ];
 }
