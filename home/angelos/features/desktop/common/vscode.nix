cde92cad529b22c064095a3d18fe2b9ce36d9426
Angelos Stylianidis <agathangelos.stylianidis@gmail.com>
2024-09-11 11:47:26 +0300

    vscode plugins

--- home/angelos/features/desktop/common/vscode.nix
+++ home/angelos/features/desktop/common/vscode.nix
@@ -12,6 +12,8 @@
       ms-vscode.cmake-tools
       vadimcn.vscode-lldb
       jnoortheen.nix-ide
+      mkhl.direnv
+      vadimcn.vscode-lldb
     ]
     ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
       {
@@ -19,6 +21,18 @@
         publisher = "aleohq";
         version = "0.32.1";
         sha256 = "sha256-aY89FHGGAmymiDWBUWbOqyWVs3VEDDWnSBEHl/kOURQ=";
+      }
+      # {
+      #   name = "esbuild-problem-matchers";
+      #   publisher = "connor4312";
+      #   version = "0.0.3";
+      #   sha256 = "sha256-esLqld9bTlFJ9/qx4qlIx1F3MtMZpr8G9/FMqGvAvtg=";
+      # }
+      {
+        name = "mock-debug";
+        publisher = "ms-vscode";
+        version = "0.52.0";
+        sha256 = "sha256-l+00y6xJt7vAS8kv6PjBMwUJCNG1ZgFsMQoGXlC6J88=";
       }
     ];
   };
